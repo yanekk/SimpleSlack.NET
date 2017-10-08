dd27f234cfc25c071091fb573ee945955d0fd958
Jan Królikowski <yanekk86@gmail.com>
2017-10-08 18:55:28 +0200

    Add samples project & posting message sample

---
+++ SimpleSlack/SimpleSlack.WebAPI.Samples/Groups.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace SimpleSlack.WebAPI.Samples
+{
+    [TestFixture]
+    public class Groups
+    {
+        [Test]
+        public void PostMessageOnGroup()
+        {
+            // initialize client with app token
+            var client = new SlackWebApiClient("## api-token ##");
+
+            // get group list
+            var groups = client.Groups.List();
+
+            // find specific group
+            var myGroup = groups.Single(g => g.Name == "my-group");
+
+            // post a message
+            client.Chat.PostMessage(myGroup, "This is the message from bot.");
+        }
+    }
+}
